b2c60525219a0f58b8ebd5437276fb6b1003c352
shivani-0515 <shivanichaudhary05072001@gmail.com>
2022-06-21 19:37:25 +0530

    modified

--- src/controller/blogController.js
+++ src/controller/blogController.js
@@ -3,8 +3,29 @@ const authorModel = require("../model/authorModel")
 const blogModel = require("../model/blogModel")
 
 const createBlog = async function (req,res){
-    let data = req.body 
+  try{  let data = req.body 
+    let id = req.authorId
+ let author = await authorModel.findById(id)
+ if(!author){res.status(400).send({status:false,msg:"author doesn't exist"})}
     let savedData = await blogModel.create(data)
-    res.send({msg : savedData})
+    res.status(201).send({msg : savedData})}
+  
+  catch(err){res.status(500).send({msg:err.message})}}
+module.exports.createBlog = createBlog
+ 
+const getBlog = async function(req,res){
+try{
+    let Id = req.params.authorId
+    let data = await blogModel.find({authorId:`$`, isDeleted:false,isPublished:true})
+
+
+
+
+
+
 }
-module.exports.createBlog=createBlog
+
+
+
+
+}
