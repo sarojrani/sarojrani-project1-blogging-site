8ab62f3f78f3bf9f6584da0d9b05ec2c923cba8c
shivani-0515 <shivanichaudhary05072001@gmail.com>
2022-06-21 16:37:31 +0530

    modified

--- src/controller/blogController.js
+++ src/controller/blogController.js
@@ -1,10 +1,10 @@
-
+const authorModel = require("../model/authorModel")
 
 const blogModel = require("../model/blogModel")
 
 const createBlog = async function (req,res){
     let data = req.body 
-    let savedData = await authorModel.create(data)
+    let savedData = await blogModel.create(data)
     res.send({msg : savedData})
 }
 module.exports.createBlog=createBlog
